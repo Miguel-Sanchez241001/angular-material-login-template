97750d88243b5b04b257ab76949da4a1f6b6b559
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2023-12-21 16:32:44 -0500

    primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-login-template';
+
+  showSidebar = false;
+
+  openSidebar() {
+    this.showSidebar = true;
+  }
+
+  closeSidebar() {
+    this.showSidebar = false;
+  }
 }
